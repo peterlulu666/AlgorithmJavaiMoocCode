676010c4161fe805c89ffe362b8295dd1284f320
vpslinuxinstall <shoparoundwyz@gmail.com>
2019-12-24 15:13:35 -0500

    commit

--- src/LinkedList/LL.java
+++ src/LinkedList/LL.java
@@ -2,15 +2,32 @@ package LinkedList;
 
 public interface LL<E> {
     void add(int index, E data);
+
     void addFirst(E data);
+
     void addLast(E data);
+
     boolean contains(E data);
+
     E get(int index);
+
     E getFirst();
+
     E getLast();
+
     int getSize();
+
     boolean isEmpty();
 
+    E removeNode(E data);
+
+    E remove(int index);
+
+    E removeFirst();
+
+    E removeLast();
+
+    void set(int index, E data);
 
 
 }
--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -63,32 +63,163 @@ public class LinkedList<E> implements LL<E> {
 
     @Override
     public boolean contains(E data) {
+        Node<E> tmpPointer = head;
+        while (tmpPointer != null) {
+            if (tmpPointer.data.equals(data)) {
+                return true;
+
+            }
+            tmpPointer = tmpPointer.next;
+
+        }
         return false;
+
     }
 
     @Override
     public E get(int index) {
-        return null;
+        if (index < 0 || index > currentSize) {
+            throw new IllegalArgumentException("The index should be between 0 and linked list size. ");
+
+        }
+        Node<E> tmpPointer = head;
+        for (int countMove = 1; countMove <= index; countMove++) {
+            tmpPointer = tmpPointer.next;
+
+        }
+        return tmpPointer.data;
+
     }
 
     @Override
     public E getFirst() {
-        return null;
+        return head.data;
+
     }
 
     @Override
     public E getLast() {
-        return null;
+        Node<E> tmpPointer = head;
+        for (int i = 1; i <= currentSize - 1; i++) {
+            tmpPointer = tmpPointer.next;
+
+        }
+        return tmpPointer.data;
+
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return currentSize;
+
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return head == null;
+
+    }
+
+    @Override
+    public E removeNode(E data) {
+        if (head == null) {
+            return null;
+
+        }
+        if (head.data.equals(data)){
+            return removeFirst();
+
+        }
+        Node<E> tmpPointer = head;
+        Node<E> prevPointer = null;
+        while (tmpPointer != null) {
+            if (tmpPointer.data.equals(data)) {
+                prevPointer.next = tmpPointer.next;
+                currentSize--;
+                return data;
+
+            }
+            prevPointer = tmpPointer;
+            tmpPointer = tmpPointer.next;
+
+        }
+        return null;
+
+    }
+
+
+    @Override
+    public E remove(int index) {
+        if (index < 0 || index > currentSize) {
+            throw new IllegalArgumentException("The index should be between 0 and linked list size. ");
+
+        }
+        Node<E> tmpPointer = head;
+        if (index == 0) {
+            return removeFirst();
+
+        }
+        for (int i = 1; i <= index - 1; i++) {
+            tmpPointer = tmpPointer.next;
+
+        }
+        E removedData = (E) tmpPointer.next.data;
+        tmpPointer.next = tmpPointer.next.next;
+        currentSize--;
+        return removedData;
+
+    }
+
+    @Override
+    public E removeFirst() {
+        if (head == null) {
+            return null;
+
+        }
+        E removedData = head.data;
+        Node<E> tmpPointer = head;
+        tmpPointer = tmpPointer.next;
+        head = tmpPointer;
+        currentSize--;
+        return removedData;
+
+    }
+
+    @Override
+    public E removeLast() {
+        if (head == null) {
+            return null;
+
+        }
+        if (head.next == null) {
+            return removeFirst();
+
+        }
+        Node<E> tmpPointer = head;
+        for (int i = 1; i <= currentSize - 2; i++) {
+            tmpPointer = tmpPointer.next;
+
+        }
+        E removedData = (E) tmpPointer.next.data;
+        tmpPointer.next = null;
+        currentSize--;
+        return removedData;
+
+    }
+
+    @Override
+    public void set(int index, E data) {
+        if (index < 0 || index > currentSize - 1) {
+            throw new IllegalArgumentException("The index should be between 0 and linked list size. ");
+
+        }
+        Node<E> tmpPointer = head;
+        for (int countMove = 1; countMove <= index; countMove++) {
+            tmpPointer = tmpPointer.next;
+
+        }
+        tmpPointer.data = data;
+
     }
 
     public void show() {
@@ -98,6 +229,7 @@ public class LinkedList<E> implements LL<E> {
             tmpPointer = tmpPointer.next;
 
         }
+        System.out.println(" null ");
 
     }
 
@@ -106,7 +238,22 @@ public class LinkedList<E> implements LL<E> {
         linkedList.addFirst(100);
         linkedList.addFirst(200);
         linkedList.addLast(300);
-        linkedList.add(3, 500);
+        linkedList.add(2, 500);
+        linkedList.show();
+        System.out.println(linkedList.contains(300));
+        System.out.println(linkedList.get(1));
+        System.out.println(linkedList.getFirst());
+        System.out.println(linkedList.getLast());
+        System.out.println(linkedList.getSize());
+        System.out.println(linkedList.isEmpty());
+        System.out.println(linkedList.remove(2));
+        System.out.println(linkedList.removeFirst());
+        System.out.println(linkedList.removeLast());
+        linkedList.addFirst(800);
+        linkedList.addFirst(500);
+        linkedList.set(2, 600);
+        linkedList.show();
+        System.out.println(linkedList.removeNode(1000));
         linkedList.show();
 
 
